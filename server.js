d966c0821f35eb1819db01a0842d88ade28bbf0e
Shiva prasad reddy Goli <153407405+golishivaprasadreddy@users.noreply.github.com>
2025-03-31 08:03:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const QRModel = require("./models/QRModel");
-const User = require('./models/UserModel');
+const User = require('./models/userModel');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
